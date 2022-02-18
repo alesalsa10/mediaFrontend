780be9d236d9b5f7bc41a97f7c39f5ab98f1fa8c
alesalsa10 <alexandersalsa10@gmail.com>
2022-02-18 18:28:21 -0500

    searchBar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,64 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { Container } from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { borderRadius, padding } from '@mui/system';
+
+const StyledTextField = withStyles({
+  root: {
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderRadius: `90px`,
+      },
+    },
+  },
+})(TextField);
+
 
 export default function Search() {
+
   return (
-    <Box
-      border={1}
-      borderColor='red'
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
-    >
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography variant='h4' component='h4'>
-            Welcome.
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField fullWidth label='fullWidth' id='fullWidth' />
-        </Grid>
+    <Grid container justifyContent='center'>
+      <Grid item xs={12} md={8} border={1} borderColor='blue' p={8}>
+        <Typography variant='h4' component='h4'>
+          Welcome.
+        </Typography>
+        <Typography variant='h4' component='h4' pb={5}>
+          Movies, TV shows, books, and people. Explore now
+        </Typography>
+        <StyledTextField
+          className='inputRounded'
+          fullWidth
+          label='Search'
+          id='search'
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <div
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    height: '100%',
+                    backgroundColor: 'red',
+                    color: '#5072a7',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    paddingLeft: '2rem',
+                    paddingRight: '2rem',
+                    borderRadius: '90px',
+                  }}
+                >
+                  Search
+                </div>
+              </InputAdornment>
+            ),
+          }}
+        />
       </Grid>
-    </Box>
+    </Grid>
   );
 }
