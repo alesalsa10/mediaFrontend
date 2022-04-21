425f2fa2e50fde86aa1821cf7e652b81c63ac401
alesalsa10 <alexandersalsa10@gmail.com>
2022-04-21 13:55:47 -0400

    minor change

--- src/services/setUpInterceptors.js
+++ src/services/setUpInterceptors.js
@@ -8,26 +8,18 @@ const setup = (store) => {
       return res;
     },
     async (err) => {
-        console.log('fdsf')
       const originalConfig = err.config;
-      if (originalConfig.url !== '/auth/signin' && err.response) {
-          console.log('expired token')
+      if (
+        (originalConfig.url !== '/auth/signin' ||
+          originalConfig.url !== '/auth/register') &&
+        err.response
+      ) {
+        console.log('expired token');
         // Access Token was expired
         if (err.response.status === 401 && !originalConfig._retry) {
           originalConfig._retry = true;
-        //   try {
-        //     const rs = await axiosInstance.post('/auth/refreshtoken', {
-        //       refreshToken: TokenService.getLocalRefreshToken(),
-        //     });
-        //     const { accessToken } = rs.data;
-        //     dispatch(refreshToken(accessToken));
-        //     TokenService.updateLocalAccessToken(accessToken);
-        //     return axiosInstance(originalConfig);
-        //   } catch (_error) {
-        //     return Promise.reject(_error);
-        //   }
-        dispatch(refreshToken())
-        return axiosInstance(originalConfig);
+          dispatch(refreshToken());
+          return axiosInstance(originalConfig);
         }
       }
       return Promise.reject(err);
